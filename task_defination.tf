003012ea302ff25db2ed81dd147b5bd1a1e74ecb
chanxxx1912 <109349749+chanxxx1912@users.noreply.github.com>
2022-11-28 18:27:28 +0530

    Create task_defination.tf

---
+++ task_defination.tf
@@ -0,0 +1,39 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+    name  = var.cluster_name
+}
+
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family             = "Terraform-ECS-Demo"
+  execution_role_arn = "arn:aws:iam::135634294856:role/ecsTaskExecutionRole"
+  memory             = 1024
+  cpu                = 512
+  container_definitions = jsonencode([
+    {
+      name      = "Deployment"
+      image     = "nginx:latest"
+      cpu       = 512
+      memory    = 1024
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "Deploymnet"
+    }
+  )
+}
+
+resource "aws_ecs_service" "nginx" {
+  name            = "ecs"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 2
+}
